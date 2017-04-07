df4f08b3858c3f732b5a0f93ff7ebad5c4b08c0b
Soo I. Kim <kimsooil@gmail.com>
2017-04-07 11:41:55 -0400

    Update prob2-xml-rpc.groovy

--- prob2-xml-rpc.groovy
+++ prob2-xml-rpc.groovy
@@ -1,7 +1,7 @@
 @Grab('org.codehaus.groovy:groovy-xmlrpc:0.8')
 import groovy.net.xmlrpc.*
 
-def imgUrl = 'http://office3.docuvantage.com:8080/optixapi/demo1' 
+def imgUrl = 'http://office3.docuvXXXX.com:8080/optixapi/demo1' 
 
 def imgXmlRpc = new XMLRPCServerProxy(imgUrl)
 
